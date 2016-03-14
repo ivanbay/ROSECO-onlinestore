6518d10454dbb189559b1d6f1ba297c6d93ac748
aiipee <aiipee05@gmail.com>
2016-03-14 13:25:39 +0800

    Modified productController

--- app/controllers/productController.php
+++ app/controllers/productController.php
@@ -1,30 +1,28 @@
 <?php
 
 /**
-* 
-*/
-class productController extends baseController
-{
-	
-	protected $layout = 'layouts.master';
+ * 
+ */
+class productController extends baseController {
 
-	public function __construct(){
-		$this->beforeFilter(function(){
-            if(!Auth::check()) {
+    protected $layout = 'layouts.master';
+
+    public function __construct() {
+        $this->beforeFilter(function() {
+            if (!Auth::check()) {
                 return Redirect::to('home')->with('login_error', '<div class="alert alert-danger text-center">Your session has expired. Please login to continue.</div>');
             }
         });
-	}
+    }
 
-	public function getView($prod_id)
-	{
-		$product = new products();
-		$list = $product->listSpecificProducts($prod_id);
+    public function getView($prod_id) {
+        $product = new products();
+        $list = $product->listSpecificProducts($prod_id);
 
-		/*print("<pre>" . print_r($list, 1) . "</pre>");
-		return "asd";*/
+        /* print("<pre>" . print_r($list, 1) . "</pre>");
+          return "asd"; */
 
-		$this->layout->content = View::make('view_product')->with('products', $list);
-	}
+        $this->layout->content = View::make('view_product')->with('products', $list);
+    }
 
 }
